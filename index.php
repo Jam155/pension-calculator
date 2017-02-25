97ed59cb30b613d404e5764cf64480e0306c7408
James Lendrem <James@kinocreative.co.uk>
2017-02-25 09:46:03 +0000

    Setup frontend files

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<title>
+			Pension Calculator
+		</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+
+	</head>
+
+	<body>
+
+		<h1>Pension Calculator</h1>
+
+		<div class="body">
+
+			<div class="form">
+
+				Form
+
+			</div><!--
+
+			--><div class="view">
+
+				View
+
+			</div>
+
+		</div>
+
+	</body>
+
+</html>
