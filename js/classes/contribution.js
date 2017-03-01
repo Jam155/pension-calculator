5ecbd4fa6e26725a1c4c63b22b33ae357ad74247
James Lendrem <James@kinocreative.co.uk>
2017-03-01 19:21:54 +0000

    Create Contribution object

---
+++ js/classes/contribution.js
@@ -0,0 +1,29 @@
+
+class Contribution {
+
+	constructor(date, amount) {
+
+		this.date = new Date(date);
+		this.amount = amount;
+
+	}
+
+	public static compare(a, b) {
+
+		if (a.date < b.date) {
+
+			return -1;
+
+		} else if (a.date > b.date) {
+
+			return 1;
+
+		} else {
+
+			return 0;
+
+		}
+
+	}
+
+}
